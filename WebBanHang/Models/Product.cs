dca7ff54cd480841851744ac1211b86f1fb84aa5
ADMIN <ADMIN@MSI>
2025-06-10 16:10:44 +0700

    hoanthanh

--- WebBanHang/Models/Product.cs
+++ WebBanHang/Models/Product.cs
@@ -13,7 +13,7 @@ namespace WebBanHang.Models
         [Required, StringLength(200)]
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
+        public double Price { get; set; }       
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public virtual Category Category { set; get; } 
